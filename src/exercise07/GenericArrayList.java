a7bcab2945fd6eafc3d0ed50df93c040f9e59f9c
katarinastevanovic <ka591ste@htwg-konstanz.de>
2023-12-06 13:23:26 +0100

    exercise07

--- src/exercise07/GenericArrayList.java
+++ src/exercise07/GenericArrayList.java
@@ -14,7 +14,8 @@ public class GenericArrayList<T> implements ISimpleGenericList<T> {
     public GenericArrayList(Class<T> type) {
         this.size = 0;
         this.type = type;
-        this.data = createGenericArray(type, 2); // this is the equivalent for data = new T[size]
+        // this is the equivalent for data = new T[size]
+        this.data = createGenericArray(type, 2);
     }
     // This is a limitation of Java's type system:
     // Types are erased during compilation,
@@ -31,6 +32,8 @@ public class GenericArrayList<T> implements ISimpleGenericList<T> {
     }
 
     @Override
+    // Gibt das Element an der angegebenen Position zurück
+    // Wirft eine NoSuchElementException, wenn kein Element an der angegebenen Position existiert
     public T get(int pos) throws NoSuchElementException {
         if (pos < 0 || pos >= size) {
             throw new NoSuchElementException();
@@ -40,19 +43,25 @@ public class GenericArrayList<T> implements ISimpleGenericList<T> {
 
 
     @Override
+    // IllegalArgumentException für ungültige Positionen
     public void add(int pos, T element) throws IllegalArgumentException {
         if(pos < 0 || pos > size) {
             throw new IllegalArgumentException();
         }
+        //Überprüfen ob array voll ist
         if(size == data.length) {
             T[] newData = createGenericArray(type, data.length * 2);
             System.arraycopy(data, 0, newData, 0, data.length);
             data = newData;
         }
 
+        //durchläuft die Elemente des Arrays rückwärts, beginnt beim letzten Element (size - 1) bis pos
         for (int i = size - 1; i >= pos; i--) {
+        // das Element wird an der Position i um eine Position nach rechts verschoben
             data[i + 1] = data[i];
         }
+        //das neue Element wird in das Array an der gewünschten Position eingefügt,
+        // und die Größe der Liste wird entsprechend erhöht
         data[pos] = element;
         size++;
 
@@ -62,12 +71,24 @@ public class GenericArrayList<T> implements ISimpleGenericList<T> {
     public String toCommaSeparatedString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < size; i++) {
+        //  Vor jedem Element außer dem ersten wird ein Komma eingefügt
             if (i > 0) {
                 sb.append(",");
             }
+        // das aktuelle Element der Liste wird dem StringBuilder hinzugefügt
             sb.append(data[i]);
         }
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        // Beispiel für die Verwendung der GenericArrayList mit dem Typ String
+        GenericArrayList<String> stringList = new GenericArrayList<>(String.class);
+        stringList.add(0, "Element 1"); // Hinzufügen an Index 0
+        stringList.add(1, "Element 2"); // Hinzufügen an Index 1
+        System.out.println("Size: " + stringList.size());
+        System.out.println("Element at index 1: " + stringList.get(1));
+        System.out.println("Comma-separated string: " + stringList.toCommaSeparatedString());
     }
 }
 
--- src/exercise07/StringArrayList.java
+++ src/exercise07/StringArrayList.java
@@ -19,6 +19,8 @@ public class StringArrayList implements ISimpleStringList {
     }
 
     @Override
+    // Gibt das Element an der angegebenen Position zurück
+    // Wirft eine NoSuchElementException, wenn kein Element an der angegebenen Position existiert
     public String get(int pos) throws NoSuchElementException {
         if (pos < 0 || pos >= size) {
             throw new NoSuchElementException();
@@ -28,18 +30,25 @@ public class StringArrayList implements ISimpleStringList {
 
 
     @Override
+    // IllegalArgumentException für ungültige Positionen
     public void add(int pos, String element) throws IllegalArgumentException {
         if(pos < 0 || pos > size) {
             throw new IllegalArgumentException();
         }
+        //Überprüfen ob array voll ist
         if(size == data.length) {
             String[] newData = new String[data.length * 2];
             System.arraycopy(data, 0, newData, 0, data.length);
             data = newData;
         }
+        //verschiebt Elemente um Platz zu machen
+        //durchläuft die Elemente des Arrays rückwärts, beginnt beim letzten Element (size - 1) bis pos
         for (int i = size - 1; i >= pos; i--) {
+        //i wird um eine Position nach rechts verschoben
             data[i + 1] = data[i];
         }
+       //das neue Element wird an der gewünschten Position eingefügt
+       // und die Größe der Liste wird erhöht
         data[pos] = element;
         size++;
 
@@ -47,13 +56,18 @@ public class StringArrayList implements ISimpleStringList {
 
     @Override
     public String toCommaSeparatedString() {
+        // wenn Liste leer ist, dann wird eine leere Zeichenkette zurückgegeben
         if(size == 0) {
             return"";
         }
         StringBuilder sb = new StringBuilder();
+        // fügt das erste Element der Liste dem StringBuilder hinzu
         sb.append(data[0]);
+        //Durchläuft die restlichen Elemente der Liste
+        // und fügt sie dem StringBuilder hinzu, vor jedem Element wird ein Komma eingefügt
         for (int i = 1; i < size; i++) {
-            sb.append(",").append(data[i]);
+            sb.append(",");
+            sb.append(data[i]);
         }
         return sb.toString();
     }
